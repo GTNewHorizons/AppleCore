33e42adc990a2eac25383d7aaa61f5a1eedc5522
Wilhelm Schuster <dev@rot13.io>
2022-11-06 00:25:38 +0100

    spotlessApply

--- src/main/java/squeek/applecore/mixins/minecraft/BlockCropsMixin.java
+++ src/main/java/squeek/applecore/mixins/minecraft/BlockCropsMixin.java
@@ -1,6 +1,7 @@
 package squeek.applecore.mixins.minecraft;
 
 import cpw.mods.fml.common.eventhandler.Event;
+import java.util.Random;
 import net.minecraft.block.BlockBush;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.world.World;
@@ -8,8 +9,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import squeek.applecore.api.AppleCoreAPI;
-
-import java.util.Random;
 
 @Mixin(BlockCrops.class)
 public class BlockCropsMixin extends BlockBush {
@@ -27,13 +26,17 @@ public class BlockCropsMixin extends BlockBush {
     public void updateTick(World world, int blockX, int blockY, int blockZ, Random random) {
         super.updateTick(world, blockX, blockY, blockZ, random);
 
-        Event.Result allowGrowthResult = AppleCoreAPI.dispatcher.validatePlantGrowth(this, world, blockX, blockY, blockZ, random);
-        if (allowGrowthResult == Event.Result.ALLOW || (allowGrowthResult == Event.Result.DEFAULT && world.getBlockLightValue(blockX, blockY + 1, blockZ) >= 9)) {
+        Event.Result allowGrowthResult =
+                AppleCoreAPI.dispatcher.validatePlantGrowth(this, world, blockX, blockY, blockZ, random);
+        if (allowGrowthResult == Event.Result.ALLOW
+                || (allowGrowthResult == Event.Result.DEFAULT
+                        && world.getBlockLightValue(blockX, blockY + 1, blockZ) >= 9)) {
             int metadata = world.getBlockMetadata(blockX, blockY, blockZ);
             if (metadata < 7) {
                 float f = this.func_149864_n(world, blockX, blockY, blockZ);
 
-                if (allowGrowthResult == Event.Result.ALLOW || (allowGrowthResult == Event.Result.DEFAULT && random.nextInt((int) (25.0F / f) + 1) == 0)) {
+                if (allowGrowthResult == Event.Result.ALLOW
+                        || (allowGrowthResult == Event.Result.DEFAULT && random.nextInt((int) (25.0F / f) + 1) == 0)) {
                     world.setBlockMetadataWithNotify(blockX, blockY, blockZ, metadata + 1, 2);
                     AppleCoreAPI.dispatcher.announcePlantGrowth(this, world, blockX, blockY, blockZ, metadata);
                 }

6a45915e7e217549e285257cd8c50e525a7111fc
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-08-23 14:14:50 +0200

    Fix by removing alpha check

--- src/main/java/squeek/applecore/client/HUDOverlayHandler.java
+++ src/main/java/squeek/applecore/client/HUDOverlayHandler.java
@@ -174,16 +174,12 @@ public class HUDOverlayHandler {
     }
 
     public static void enableAlpha(float alpha) {
-        if (alpha == 1f) return;
-
         GL11.glColor4f(1f, 1f, 1f, alpha);
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
     }
 
     public static void disableAlpha(float alpha) {
-        if (alpha == 1f) return;
-
         GL11.glDisable(GL11.GL_BLEND);
         GL11.glColor4f(1f, 1f, 1f, 1f);
     }
